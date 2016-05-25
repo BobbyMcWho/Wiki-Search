cd0babe0aa48a08f1dfe694274d1a8c9c911fe16
Robert McDonald <BobbyMcWho@users.noreply.github.com>
2016-05-24 22:17:55 -0400

    Add

--- js/main.js
+++ js/main.js
@@ -14,19 +14,20 @@ $(document).ready(function() {
       }),
       complete: function() {
         //Run our loop on the queryResults
-        loopResults(queryResults);
+        if (queryResults[1] == ""){$('#father').append('<a class="card" target="_blank">' + '<div class="son"><h2 class="cardHead">No results found.</h2>' + '<br>' + '<p>Try another search query!</p></div></a>');;}
+        else loopResults(queryResults);
       }
     });
   }
-//Define Loop that will append divs with query information
+//Define Loop that will append divs with query information 
   function loopResults(a) {
     var i = 0;
-    for (i = 0; i < a[1].length; i++) {
+    for (i = 0; i < a[1].length;i++) {
       $('#father').append('<a class="card" target="_blank" href=' + a[3][i] + '>' + '<div class="son"><h2 class="cardHead">' + a[1][i] + '</h2>' + '<br>' + '<p>' + a[2][i] + '</p></div></a>');
     }
   }
-  //On Search Icon click:
-  //1. Hide tooltip (important on mobile)
+  //On Search Icon click: 
+  //1. Hide tooltip (important on mobile) 
   //2. Hide "Click to search" text (only shown on mobile)
   //3. Add Class "Rounded-focus to prompt animation
   //4. Fade out the Search Icon, and upon completion, fade in the Close icon
@@ -38,7 +39,7 @@ $(document).ready(function() {
       $("#close").fadeIn(200);
     });
   });
-  //On Search field blur (don't need to define close icon on.click due to Text field losing blur when clicking it anyways:
+  //On Search field blur (don't need to define close icon on.click due to Text field losing blur when clicking it anyways: 
   //1. Show "Click to search" text (only shown on mobile)
   //2. Remove Class "Rounded-focus to prompt animation close
   //4. Fade out the Close Icon, and upon completion, fade in the Search icon
@@ -51,8 +52,8 @@ $(document).ready(function() {
   });
   //When text input is active, when enter is pressed:
   //1. Set value of text box to a variable, vall
-  //2. Set our global query variable to the encodedURIcomponent of vall.
-  //3. Clear all existing children divs in the #father div.
+  //2. Set our global query variable to the encodedURIcomponent of vall. 
+  //3. Clear all existing children divs in the #father div. 
   //4. Run our wikiSearch function
   //5. Animate our main container div to have smaller margin-top
   $('#search').keypress(function(e) {
