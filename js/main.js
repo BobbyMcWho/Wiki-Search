a2c7a5839ad60c5359037c9c9e6d94ebc59d02d8
BobbyMcWho <BobbyMcWho@gmail.com>
2017-03-17 19:38:17 -0400

    Updated footer

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-$(document).ready(function() {
- //Set global variables
+$(document).ready(function () {
+  //Set global variables
   var query = "";
   var queryResults = [];
-//Query Wikipedia API for input value (as var query)
+  //Query Wikipedia API for input value (as var query)
   function wikiSearch() {
     $.ajax({
       type: 'GET',
       crossDomain: true,
       dataType: 'json',
       url: ('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&limit=10&namespace=0&format=json&callback=?'),
-      success: (function(e) {
+      success: (function (e) {
         queryResults = e;
       }),
-      complete: function() {
+      complete: function () {
         //Run our loop on the queryResults
-        if (queryResults[1] == ""){$('#father').append('<a class="card" target="_blank">' + '<div class="son"><h2 class="cardHead">No results found.</h2>' + '<br>' + '<p>Try another search query!</p></div></a>');;}
-        else loopResults(queryResults);
+        if (queryResults[1] == "") {
+          $('#father').append('<a class="card" target="_blank">' + '<div class="son"><h2 class="cardHead">No results found.</h2>' + '<br>' + '<p>Try another search query!</p></div></a>');;
+        } else loopResults(queryResults);
       }
     });
   }
-//Define Loop that will append divs with query information 
+  //Define Loop that will append divs with query information 
   function loopResults(a) {
     var i = 0;
-    for (i = 0; i < a[1].length;i++) {
+    for (i = 0; i < a[1].length; i++) {
       $('#father').append('<a class="card" target="_blank" href=' + a[3][i] + '>' + '<div class="son"><h2 class="cardHead">' + a[1][i] + '</h2>' + '<br>' + '<p>' + a[2][i] + '</p></div></a>');
     }
   }
@@ -31,11 +32,11 @@ $(document).ready(function() {
   //2. Hide "Click to search" text (only shown on mobile)
   //3. Add Class "Rounded-focus to prompt animation
   //4. Fade out the Search Icon, and upon completion, fade in the Close icon
-  $("#searchIcon").click(function() {
+  $("#searchIcon").click(function () {
     $('[data-toggle="tooltip"]').tooltip('hide');
     $('.searchMobile').css("display", "none");
     $("#search").toggleClass("rounded-focus", "rounded:focus").focus();
-    $("#searchIcon").fadeOut(200, function() {
+    $("#searchIcon").fadeOut(200, function () {
       $("#close").fadeIn(200);
     });
   });
@@ -43,9 +44,9 @@ $(document).ready(function() {
   //1. Show "Click to search" text (only shown on mobile)
   //2. Remove Class "Rounded-focus to prompt animation close
   //4. Fade out the Close Icon, and upon completion, fade in the Search icon
-  $("#search").blur(function() {
+  $("#search").blur(function () {
     $("#search").removeClass("rounded-focus", "rounded:focus");
-    $("#close").fadeOut(200, function() {
+    $("#close").fadeOut(200, function () {
       $("#searchIcon").fadeIn(200);
       $('.searchMobile').css("display", "");
     });
@@ -56,7 +57,7 @@ $(document).ready(function() {
   //3. Clear all existing children divs in the #father div. 
   //4. Run our wikiSearch function
   //5. Animate our main container div to have smaller margin-top
-  $('#search').keypress(function(e) {
+  $('#search').keypress(function (e) {
     if (e.keyCode == 13 && $('#search').val() !== "") {
       var vall = $('#search').val();
       query = encodeURIComponent(vall);
